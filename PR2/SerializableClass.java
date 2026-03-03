ede05f27f56896c4bfacb2eda6c95f8aa783d595
Raakkaani <lirodaki345@gmail.com>
2026-03-03 17:51:14 +0200

    зміни

---
+++ PR2/SerializableClass.java
@@ -0,0 +1,9 @@
+import java.io.Serializable;
+
+public class SerializableClass implements Serializable{
+    private static final long serialVersionUID = 1;
+    private double parameter1;
+    private double parameter2;
+    private double result;
+
+}
